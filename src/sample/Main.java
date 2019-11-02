06914fe236ce8d5f069fce7102dfc885c3472a7a
ersincebi <ersincebi.1995@gmail.com>
2019-11-02 22:52:41 +0300

    design part is finished

--- src/sample/Main.java
+++ src/sample/Main.java
@@ -10,10 +10,11 @@ public class Main extends Application {
 
     @Override
     public void start(Stage primaryStage) throws Exception{
-        Parent root = FXMLLoader.load(getClass().getResource("sample.fxml"));
-        primaryStage.setTitle("Hello World");
-        primaryStage.setScene(new Scene(root, 300, 275));
-        primaryStage.show();
+        Parent root = FXMLLoader.load(getClass().getResource("mailDisplayer.fxml"));
+        primaryStage.setTitle("Send Receive Mail");
+		primaryStage.setScene(new Scene(root));
+		primaryStage.setResizable(false);
+		primaryStage.show();
     }
 
 
